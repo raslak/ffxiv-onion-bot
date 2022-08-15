48f03690ca42011594f8219dcf44943c4c670b7f
Ben Hancock <ben.hancock@gmail.com>
2022-08-15 09:51:49 -0400

    add option for world, data center, or region to be specified in request

--- bot.py
+++ bot.py
@@ -14,30 +14,29 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='commands')
 async def commands(ctx):
     response = "```\n"
-    response += "+------------------+---------------------------------------------------------+\n"
-    response += "| Command          | Description                                             |\n"
-    response += "+------------------+---------------------------------------------------------+\n"
-    response += "| onion            | Five lowest thavnairian onion prices in NA data centers.|\n"
-    response += "| prices <item_id> | Five lowest prices for <item_id> in NA data centers.    |\n"
-    response += "+------------------+---------------------------------------------------------+\n"
+    response += "+-----------------------------------------------+------------------------+\n"
+    response += "| Command                                       | Example                |\n"
+    response += "+-----------------------------------------------+------------------------+\n"
+    response += "| onion <world_dc_region>                       | !onion primal          |\n"
+    response += "| prices <item_ids> <world_dc_region, optional> | !prices 8166 ultros    |\n"
+    response += "+------------------+-----------------------------------------------------+\n"
     response += "```"
 
     await ctx.send(response)
 
 @bot.command(name='onion')
-async def onion(ctx):
+async def onion(ctx, world_dc_region="North-America"):
     try:
-        response = find_cheapest_onion()
+        response = find_cheapest_onion(item_ids=8166, world_dc_region=world_dc_region)
     except Exception:
         response = 'Unable to find prices'
     
     await ctx.send(response)
 
 @bot.command(name='prices')
-async def prices(ctx, arg):
-    item_id = arg
+async def prices(ctx, item_ids, world_dc_region="North-America"):
     try:
-        response = find_cheapest_onion(item_id)
+        response = find_cheapest_onion(item_ids=item_ids, world_dc_region=world_dc_region)
     except Exception:
         response = 'Unable to find prices'
     
--- market.py
+++ market.py
@@ -1,16 +1,19 @@
 import requests
 
 
-def find_cheapest_onion(item_id=8166):
-    """Find five lowest prices for provided item id. If no item id is provided, defaults to 8166 ("Thavnairian Onion").
+def find_cheapest_onion(item_ids, world_dc_region="North-America"):
+    """Find five lowest prices for provided item id. By default the
+    results will be limited to all NA data centers. The optional "world_dc_region"
+    parameter can be used to specify a data center or world. 
 
     Args:
-        item_id (int, optional): Unique id for item of interest. Defaults to 8166.
+        item_id (int): Unique id for item of interest.
+        world_dc_region (str, optional): Region, data center, or world.
 
     Returns:
         str: ASCII table containing five lowest prices found for item id.
     """
-    url = f'https://universalis.app/api/v2/North-America/{item_id}'
+    url = f'https://universalis.app/api/v2/{world_dc_region}/{item_ids}'
     response = requests.get(url)
 
     data = response.json()
@@ -18,13 +21,17 @@ def find_cheapest_onion(item_id=8166):
     top_five = data['listings'][:5]
 
     results_table = "```\n"
-    results_table += f"Top five prices for https://universalis.app/market/{item_id}\n"
+    results_table += f"Top five prices for https://universalis.app/market/{item_ids}\n"
     results_table += "+---------------+---------------+-----+\n"
     results_table += "| Server        | Price         | Qty |\n"
     results_table += "+---------------+---------------+-----+\n"
 
     for item in top_five:
-        world = item['worldName']
+        if 'worldName' in item.keys():
+            world = item['worldName']
+        else:
+            world = world_dc_region.title()
+
         price = str(item['pricePerUnit'])
         qty = str(item['quantity'])
 
